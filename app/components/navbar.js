3c6c7aed251986e996df9bdc83f3b40002c24607
JHTgh <jorgenthorjussen@live.com>
2024-04-03 13:43:42 +0200

    +navbar

---
+++ app/components/navbar.js
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/bigFive.png";
+import styles from "./page.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.logo}>
+        <Image src={logo} style={{ width: "100%" }} alt="Logo" />
+      </Link>
+
+      <ul className={styles.links}>
+        <li className={styles.navElement}>
+          <Link href="https://bigfive-test.com">Test</Link>{" "}
+        </li>
+        <li className={styles.navElement}>
+          <Link href="/about">Om oss</Link>
+        </li>
+        <>
+          <li className={styles.navElement}>
+            <Link href="/profil">Profil</Link>
+          </li>
+        </>
+        <>
+          <li className={styles.navElement}>
+            <Link href="/login">Logg inn</Link>
+          </li>
+        </>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
