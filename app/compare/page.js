9bc6024052872b972974dd0d7d1dc074d9ed51d8
Kjartan Ø <119934520+Shoyo181@users.noreply.github.com>
2024-04-05 17:02:52 +0200

    test_compare
    klarer nå å finne bruker basert på uid, men mange bugs

--- app/compare/page.js
+++ app/compare/page.js
@@ -2,11 +2,46 @@
 
 import React from "react";
 import BarChart from "../components/chart";
+import { db } from "../database/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 const ComparePage = () => {
+  
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const data = await getDocs(collection(db, "users"));
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setUserList(filteredData);
+        console.log(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getUsers();
+  }, []);
+
+
+
+
   return (
     <div>
       <h1>Compare Page</h1>
+      <div> 
+      {userList.map((user) => (
+        <div key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>uID: {user.uid}</p>
+          <p>------------</p>
+        </div>
+        ))}
+      </div>
       <div>
         <BarChart />
       </div>
--- app/components/chart.js
+++ app/components/chart.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Chart from "chart.js/auto";
+import { db, queryBrukerNavn, queryBrukerScore } from "../database/firebase";
 import { Bar } from "react-chartjs-2";
+import { doc, getDoc } from 'firebase/firestore';
+
 
 const BarChart = () => {
+
   const data = {
     labels: [
       "Ekstroversjon",
@@ -12,48 +17,7 @@ const BarChart = () => {
       "Tillit",
     ],
     datasets: [
-      {
-        label: "Personlighet",
-        data: [65, 59, 80, 81, 56],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-        ],
-        borderWidth: 1,
-        barThickness: 30,
-        maxBarThickness: 50,
-      },
-      {
-        label: "Personlighet",
-        data: [55, 25, 70, 62, 45],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-        ],
-        borderWidth: 1,
-        barThickness: 30,
-        maxBarThickness: 50,
-      },
+     setEnBruker("dwf0OCy3doWpatHw8QnjpfSs4qS2"),
     ],
   };
 
@@ -69,10 +33,80 @@ const BarChart = () => {
   };
 
   return (
-    <div style={{ width: "1000px", height: "500px" }}>
-      <Bar data={data} options={options} />
+    <div>
+      <div style={{ width: "800px", height: "500px" }}>
+        <Bar data={data} options={options} />
+      </div>
     </div>
   );
 };
+
+/*
+
+export async function hentBrukerData( uID) {
+  // funksjon som henter data om bruker ved hjelp av uID (som er lik i begge databasene i firebase)
+
+
+  // Create a reference to the cities collection
+  const docRef = db.collection('users');
+
+  // Create a query against the collection
+  const queryRef = docRef.where('uid', '==', uID);
+
+  const [brukerNavn, setBrukerNavn] = useState([]);
+  const [brukerScore, setBrukerScore] = useState([]);
+
+  if (queryRef.exists()) {
+    //lagrer navnet til bruker
+    setBrukerNavn(queryRef.data().name);
+  } else {
+    console.log("No such document!");
+  }
+  //henter info om score fra en annen database
+  const docRef2 = db.collection("testRes");
+  const queryRef2 = docRef2.where('uid', '==', uID);
+  if (queryRef2.exists()) {
+    setBrukerScore(queryRef2.data());
+  } else {
+    console.log("No such document!");
+  }
+  // i brukerScore er det en kollonne i tabellen som heter uID den vil vi ha vekk
+  const score = [];
+  // det vil alltid være fem verdier vi vil ha 
+  // kan hende vi må snu denne løkken, sjekk dette !!!                                              ---!!!---
+  for (let i = 0; i < 5; i++) {
+    score.push(brukerScore[i]);
+  }
+  // slik har vi tatt vekk unødvendig data
+  // vi returnerer navn og score
+  return [brukerNavn, score];
+}*/
+
+// vi må også "bygge" hele blokken med kode til å sette opp charts altså alt i datasets
+// vi gjør dette i en funksjon
+
+export async function setEnBruker(uID) {
+
+  // vi henter info om bruker fra databasen ved hjelp av metoder laget for database
+  // legger også inn en random farge for denne brukeren i chartsen
+  return {
+    label: queryBrukerNavn(uID),
+    data: queryBrukerScore(uID),
+    backgroundColor: [
+      getRandomColors(),
+    ],
+    borderColor: [
+      "rgba(255, 255, 255, 0.5)",
+    ],
+    borderWidth: 1,
+    barThickness: 30,
+  }
+}
+
+const getRandomColors = () => {
+  // funksjon som returnerer en random farge
+  return (`rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.2)`);
+};
+
 
 export default BarChart;
