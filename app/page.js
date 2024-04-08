05cf8b8f048512ce63d5424107855002042c8d41
markusingierd <markus.ingierd@gmail.com>
2024-04-08 18:32:53 +0200

    lagt inn footer. ikke ferdig med index siden

--- app/page.js
+++ app/page.js
@@ -1,69 +1,76 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import sosial from "@/public/sosial.jpg";
-import test from "@/public/test.jpg";
-import statistikk from "@/public/statistikk.png";
+import Dame from "@/public/dame.jpg";
+import Folk from "@/public/folk.jpg";
+import Team from "@/public/team.jpg";
+import Velkommen from "@/public/velkommen.jpg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.flexEn}>
-        <div className={styles.flexVenstreEn}>
-          <Image
-            src={sosial}
-            priority={true}
-            alt="sosial"
-            className={styles.images}
-          ></Image>
-        </div>
-        <div className={styles.flexHøyreEn}>
-          <h1>Big Five personlighetstest for bedrifter</h1>
+      <div className={styles.flexVenstreEn}>
+          <h1>Velkommen til BigFives Personlighetstest!</h1>
           <p>
-            Forstå dine kollegaer og eventuelle ansatte bedre ved hjelp av denne
-            open-source personlighetstesten
+          Ønsker du å styrke din bedrifts rekrutteringsprosess?
           </p>
-          <p>Opprett en brukerkonto for din bedrift her:</p>
+          <p>Opprett en bedriftskonto her:</p>
           <ul>
             <li>
               <Link href="/profil">Opprett konto her</Link>
             </li>
           </ul>
         </div>
+        <div className={styles.flexHøyreEn}>
+        <Image
+            src={Velkommen}
+            priority={true}
+            alt="Velkommen"
+            className={styles.images}
+          ></Image>
+        </div> 
       </div>
 
       <div className={styles.flexTo}>
         <div className={styles.flexVenstreTo}>
-          <Image
-            src={test}
+        <Image
+            src={Dame}
             priority={true}
-            alt="test"
+            alt="Dame"
             className={styles.images}
           ></Image>
         </div>
+        <div className={styles.flexMidtTo}>
+        <Image
+            src={Folk}
+            priority={true}
+            alt="Folk"
+            className={styles.images}
+          ></Image> 
+        </div>
         <div className={styles.flexHøyreTo}>
-          <h1>Test deg selv</h1>
-          <p>
-            Her har du mulighet til å ta testen og teste deg selv for å se om du
-            er kompatilbel med jobben din
-          </p>
-          <p>test deg selv her:</p>
-          <ul>
-            <li>
-              <Link href="https://bigfive-test.com">Personlighetstest</Link>
-            </li>
-          </ul>
+        <Image
+            src={Team}
+            priority={true}
+            alt="Team"
+            className={styles.images}
+          ></Image> 
         </div>
       </div>
 
       <div className={styles.flexTre}>
         <div className={styles.flexVenstreTre}>
-          <Image
-            src={statistikk}
-            priority={true}
-            alt="statistikk"
-            className={styles.images}
-          ></Image>
+        <h1>Test deg selv</h1>
+        <p>
+        Vår plattform er skapt for å gi bedrifter 
+        som din en grundig forståelse av kandidatenes 
+        personlighetsegenskaper, noe som gjør det enklere 
+        å ta velinformerte beslutninger i rekrutteringsprosessen.
+        </p>
+        </div>
+        <div className={styles.flexMidtTre}>
+          <p>hei</p>
         </div>
         <div className={styles.flexHøyreTre}>
           <h1>Sammenlign ansatte eller arbeidssøkere ved rekrutering</h1>
