38497b519153d77b6636b21739b1cbd5095873ad
Miemuz <mie.ronningen@gmail.com>
2024-04-10 16:46:45 +0200

    fått inn legge til ansatte + generere testresultater på ansattID

---
+++ app/dashboard/page.js
@@ -0,0 +1,174 @@
+"use client";
+import React, { useState } from 'react';
+import { db } from "../database/firebase";
+import { addDoc, collection, doc, query, where, getDocs } from "firebase/firestore";
+
+
+
+const EmployeeForm = ({ bedriftId }) => {
+    const [employeeData, setEmployeeData] = useState({
+      name: '',
+      email: '',
+      address: '',
+      //testId: ''
+    });
+
+
+    const handleChange = (e) => {
+        setEmployeeData({
+          ...employeeData,
+          [e.target.id]: e.target.value
+        });
+      };
+      
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+
+        const ansatteCollection = collection(db, 'ansatte');
+
+        
+      // Putter inn firma som FK til ansatt. 
+        const employeeWithCompany = {
+        ...employeeData,
+        companyId: bedriftId
+      };
+      
+     const newEmployeeRef = await addDoc(ansatteCollection, employeeWithCompany);
+     const newEmployeeRefId = newEmployeeRef.id;
+
+      const testResultCollection = collection (db, "testResults");
+
+      const testResultsData = {
+        Ekstroversjon: Math.floor(Math.random()*101),
+        Nevrotisisme: Math.floor(Math.random()*101),
+        Samhandling: Math.floor(Math.random()*101),
+        Selvinnsikt: Math.floor(Math.random()*101),
+        Tillit: Math.floor(Math.random()*101),
+        ansattId: newEmployeeRef.id
+      }
+
+      await addDoc(testResultCollection, testResultsData);
+
+      // Legger til data om ansatt
+      await addDoc(ansatteCollection, employeeWithCompany);
+      // Blanker ut form etter en bruker er opprettet. 
+      setEmployeeData({
+        name: '',
+        email: '',
+        address: '',
+        //testId: '',
+
+      });
+    } catch (error) {
+      console.error('Error adding employee: ', error);
+    }
+  }
+
+  return (
+    <form onSubmit = {handleSubmit} style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px', marginTop: '20px', padding: '20px', border: '1px solid #eee', borderRadius: '10px', backgroundColor: '#f9f9f9' }}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <label htmlFor="name" style={{ marginBottom: '10px' }}>Name:</label>
+      <input type="text" id="name" value={employeeData.name} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '20px' }} />
+      <label htmlFor="email" style={{ marginBottom: '10px' }}>Email:</label>
+      <input type="email" id="email" value={employeeData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '20px' }} />
+    </div>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <label htmlFor="address" style={{ marginBottom: '10px' }}>Address:</label>
+      <input type="text" id="address" value={employeeData.address} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '20px' }} />
+     
+      <button type="submit">Add Employee</button>
+    </div>
+  </form>
+  );
+};
+
+/*const ansattListe = ({ bedriftId }) => {
+  const [ansatte, setAnsatte] = useState([]);
+
+  useEffect(() => {
+    const finnAlleAnsatte = async () => {
+      if (!bedriftId) {
+        console.error('BedriftId is null, can not fetch employees');
+        return;
+      }
+
+      try {
+        const ansatteQuery = query(
+          collection(db, 'ansatte'),
+          where('companyId', '==', bedriftId),
+        );
+        const querySnapshot = await getDocs(ansatteQuery);
+
+        const aListe = [];
+        if (querySnapshot === null) {
+          throw new Error('QuerySnapshot is null');
+        }
+        querySnapshot.forEach((doc) => {
+          if (doc === null || !doc.data()) {
+            throw new Error('Encountered null data when fetching employees');
+          }
+
+          aListe.push(doc.data());
+        });
+
+        setAnsatte(aListe);
+      } catch (error) {
+        console.error('Error fetching employees: ', error);
+      }
+    };
+
+    finnAlleAnsatte();
+  }, [bedriftId]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', width: '50%', padding: '20px', border: '1px solid #ccc' }}>
+      <h2>Employee List</h2>
+      <ul>
+        {ansatte.map((ansatt, index) => (
+          <li key={index}>
+            <strong>Name:</strong> {ansatt.name} | <strong>Email:</strong> {ansatt.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+*/
+/*
+ <label htmlFor="testId" style={{ marginBottom: '10px' }}>TestId:</label>
+      <input type="text" id="testId" value={employeeData.testId} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', marginBottom: '20px' }} />
+*/
+
+
+  const DashboardBedrift = () => {
+    const [isFormVisible, setFormVisible] = useState(false);
+    const bedriftId = '2UAfkMDJHZR58Uay3pVs1LKPQL22';
+    
+
+
+  const toggleFormVisibility = () => {
+    setFormVisible(!isFormVisible);
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '3em', fontWeight: 'bold', color: '#333', marginBottom: '20px', fontFamily: 'Roboto, sans-serif' }}>Velkommen</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h2 style={{ fontSize: '4em', fontWeight: 'bold', color: '#007bff', fontFamily: 'Roboto, sans-serif', marginBottom: '10px' }}>Legg til brukere</h2>
+        <div style={{ cursor: 'pointer' }} onClick={toggleFormVisibility}>
+          <span style={{ fontSize: '3em', color: '#007bff' }}>+</span>
+        </div>
+        <div>
+            {isFormVisible && (
+                <div>
+              <EmployeeForm bedriftId="2UAfkMDJHZR58Uay3pVs1LKPQL22" />
+              </div>
+            )}
+        </div>
+       </div>
+    </div>
+  );
+};
+export default DashboardBedrift;
