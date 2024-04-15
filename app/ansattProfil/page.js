9b9f9286fc20cb97f8e5c0b0f32b59d3d34d8cca
Anarox1111 <68535747+Anarox1111@users.noreply.github.com>
2024-04-15 14:51:13 +0200

    Viser alle resultatene fra en fetch

--- app/ansattProfil/page.js
+++ app/ansattProfil/page.js
@@ -1,51 +1,54 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const YourComponent = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://bigfive-f9cymyeb1-rubynor.vercel.app/api/result/660edc22642b4b0008934eb1/no')
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      "https://bigfive-f9cymyeb1-rubynor.vercel.app/api/result/660edc22642b4b0008934eb1/no"
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setData(data);
         setLoading(false);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
         setLoading(false);
       });
   }, []);
   let content = null;
-if (loading) {
-  content = <p>Loading...</p>;
-} else if (data) {
-  const firstResult = data.results[0];
-  const firstResultFacets = firstResult.facets;
-  
-  content = (
-    <div>
-      <h2>{data.title}</h2>
-      <p>{data.shortDescription}</p>
-      <p>{data.description}</p>
-      <p>First Result Title: {firstResult.title}</p>
-      <p>First Result Text: {firstResult.text}</p>
-      <p>First Result Score: {firstResult.scoreText}</p>
-      <h3>Facets of First Result:</h3>
-      {firstResultFacets.map((facet, facetIndex) => (
-        <div key={facetIndex}>
-          <h4>{facet.name}</h4>
-          <p>Score: {facet.score}</p>
-        </div>
-      ))}
-    </div>
-  );
-} else {
-  content = <p>No data available</p>;
-}
+  if (loading) {
+    content = <p>Loading...</p>;
+  } else if (data) {
+    content = (
+      <div>
+        <h2>{data.title}</h2>
+        <p>{data.shortDescription}</p>
+        <p>{data.description}</p>
+        {data.results.map((result, index) => (
+          <div key={index}>
+            <h3>{result.title}</h3>
+            <p>{result.text}</p>
+            <p>Score: {result.scoreText}</p>
+            <h4>Facets:</h4>
+            {result.facets.map((facet, facetIndex) => (
+              <div key={facetIndex}>
+                <h5>{facet.title}</h5>
+                <p>Score: {facet.score}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    content = <p>No data available</p>;
+  }
 
-return <div>{content}</div>;
+  return <div>{content}</div>;
 };
 
 export default YourComponent;
