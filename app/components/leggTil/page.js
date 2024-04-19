21a4f47e6a509b61397acd01659f82dbaddc7c8f
Miemuz <mie.ronningen@gmail.com>
2024-04-19 17:58:51 +0200

    endret funksjon for å få opp ansattliste - slik at det blir færre reads

--- app/components/leggTil/page.js
+++ app/components/leggTil/page.js
@@ -100,26 +100,31 @@ export const DashboardBedrift = ({bedriftId}) => {
     setIsListVisible(!isListVisible);
   };
 
-  finnAnsatteBedrift(bedriftId)
-  .then((querySnapshot) => {
-    const aListe = [];
-    querySnapshot.forEach((doc) => {
-      aListe.push(doc.data());
-    });
-    // Once you have the list of employees, you can display it or use it as needed
-    setAnsatteListe(aListe);
-
-  })
-  .catch((error) => {
-    console.error('Error fetching employees: ', error);
-  });
+  const fetchEmployeeList = () => {
+    const currentTime = new Date().getTime();
+    // Only fetch data if the list is empty or if more than 5 minutes have passed since the last fetch
+    if (!ansatteListe.length || (currentTime - lastFetchTime > 300000)) {
+      finnAnsatteBedrift(bedriftId)
+        .then((querySnapshot) => {
+          const aListe = [];
+          querySnapshot.forEach((doc) => {
+            aListe.push(doc.data());
+          });
+          setAnsatteListe(aListe);
+          setLastFetchTime(currentTime); // Update the last fetch time
+        })
+        .catch((error) => {
+          console.error('Error fetching employees: ', error);
+        });
+    }
+  };
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <h1 style={{ fontSize: '3em', fontWeight: 'bold', color: '#333', marginBottom: '20px', fontFamily: 'Roboto, sans-serif' }}>Velkommen</h1>
       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
         <div style={{ width: '50%' }}>
           <h2 style={{ fontSize: '2em', fontWeight: 'bold', color: '#007bff', fontFamily: 'Roboto, sans-serif', marginBottom: '10px' }}>Ansatte Liste</h2>
-          <ul style={{ listStyleType: 'none', padding: 0, marginTop: '10px' }}>
+          <button onClick={fetchEmployeeList}>Vis ansattliste/oppdater</button><ul style={{ listStyleType: 'none', padding: 0, marginTop: '10px' }}>
             {ansatteListe.map((ansatt, index) => (
               <li key={index} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px' }}>
                 <strong>Name:</strong> {ansatt.name} | <strong>Email:</strong> {ansatt.email}
@@ -127,6 +132,7 @@ export const DashboardBedrift = ({bedriftId}) => {
             ))}
           </ul>
         </div>
+
         <div style={{ width: '50%' }}>
           <h2 style={{ fontSize: '4em', fontWeight: 'bold', color: '#007bff', fontFamily: 'Roboto, sans-serif', marginBottom: '10px' }}>Legg til brukere</h2>
           <div style={{ cursor: 'pointer' }} onClick={toggleFormVisibility}>
