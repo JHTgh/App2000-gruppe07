58b322329d2b29dc8058fa8b186421aa99efaddf
JHTgh <jorgenthorjussen@live.com>
2024-04-21 17:01:42 +0200

    listeAnsatte CSS
    Skal finpusses når all funksjonalitet er på plass

--- app/components/alleAnsatte.js
+++ app/components/alleAnsatte.js
@@ -1,9 +1,10 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import {finnAnsatteBedrift} from '@/app/database/querys';
-import AnsattForm from '@/app/components/ansattForm';
+import React, { useEffect, useState } from "react";
+import { finnAnsatteBedrift } from "@/app/database/querys";
+import AnsattForm from "@/app/components/ansattForm";
+import styles from "./alleAnsatte.module.css";
 
-export function AlleAnsatte({bedriftId}) {
+export function AlleAnsatte({ bedriftId }) {
   const [isFormVisible, setFormVisible] = useState(false);
   const [ansatteListe, setAnsatteListe] = useState([]);
   const [isListVisible, setIsListVisible] = useState(false);
@@ -17,7 +18,7 @@ export function AlleAnsatte({bedriftId}) {
   const fetchEmployeeList = () => {
     const currentTime = new Date().getTime();
     // Only fetch data if the list is empty or if more than 5 minutes have passed since the last fetch
-    if (!ansatteListe.length || (currentTime - lastFetchTime > 300000)) {
+    if (!ansatteListe.length || currentTime - lastFetchTime > 300000) {
       finnAnsatteBedrift(bedriftId)
         .then((querySnapshot) => {
           const aListe = [];
@@ -28,33 +29,34 @@ export function AlleAnsatte({bedriftId}) {
           setLastFetchTime(currentTime); // Update the last fetch time
         })
         .catch((error) => {
-          console.error('Error fetching employees: ', error);
+          console.error("Error fetching employees: ", error);
         });
     }
   };
-  
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1 style={{ fontSize: '3em', fontWeight: 'bold', color: '#333', marginBottom: '20px', fontFamily: 'Roboto, sans-serif' }}>Velkommen</h1>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-        <div style={{ width: '50%' }}>
-          <h2 style={{ fontSize: '2em', fontWeight: 'bold', color: '#007bff', fontFamily: 'Roboto, sans-serif', marginBottom: '10px' }}>Ansatte Liste</h2>
-          <button onClick={fetchEmployeeList}>Vis ansattliste/oppdater</button><ul style={{ listStyleType: 'none', padding: 0, marginTop: '10px' }}>
-            {ansatteListe.map((ansatt, index) => (
-              <li key={index} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px' }}>
-                <strong>Name:</strong> {ansatt.name} | <strong>Email:</strong> {ansatt.email}
-              </li>
-            ))}
-          </ul>
-        </div>
 
-        <div style={{ width: '50%' }}>
-          <h2 style={{ fontSize: '4em', fontWeight: 'bold', color: '#007bff', fontFamily: 'Roboto, sans-serif', marginBottom: '10px' }}>Legg til brukere</h2>
-          <div style={{ cursor: 'pointer' }} onClick={toggleFormVisibility}>
-            <span style={{ fontSize: '3em', color: '#007bff' }}>+</span>
-          </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapperListe}>
+        <h2>Ansatte Liste</h2>
+        <button className={styles.button} onClick={fetchEmployeeList}>
+          Vis ansattliste/oppdater
+        </button>
+        <ul>
+          {ansatteListe.map((ansatt, index) => (
+            <li key={index}>
+              <strong>Name:</strong> {ansatt.name} | <strong>Email:</strong>{" "}
+              {ansatt.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.AddUserContainer}>
+        <div className={styles.AddUserWrapper} onClick={toggleFormVisibility}>
+          <h2>Legg til ny person</h2>
+          <button className={styles.button}>Legg til ny</button>
           {isFormVisible && (
-            <div>
+            <div className={styles.formContainer}>
               <AnsattForm bedriftId={bedriftId} />
             </div>
           )}
--- app/dashboard/listeAnsatte/page.js
+++ app/dashboard/listeAnsatte/page.js
@@ -1,47 +1,56 @@
 "use client";
-import { useState, useEffect } from 'react';
-import { userUId } from '@/app/dashboard/layout';
-import { addDoc, collection, doc, query, where, getDocs } from "firebase/firestore";
-import {AlleAnsatte} from '@/app/components/alleAnsatte';
+import { useState, useEffect } from "react";
+import { userUId } from "@/app/dashboard/layout";
+import styles from "./page.module.css";
+import {
+  addDoc,
+  collection,
+  doc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { AlleAnsatte } from "@/app/components/alleAnsatte";
 
 export default function ListeFremvisning() {
-    const [bruker, setBruker] = useState(null);
-    const [feil, setFeil] = useState(null);
-    
-    useEffect(() => {
-        const hentBrukerData = async () => {
-            try {
-                const uId = await userUId; // Vente på IDen
-                console.log('uID: (page) ' + uId);
-                setBruker(uId);
-            } catch (error) {
-                setFeil(error); // Håndter feil
-                console.error(error);
-            }
-        };
-    
-        hentBrukerData();
-    }, []);
+  const [bruker, setBruker] = useState(null);
+  const [feil, setFeil] = useState(null);
 
-    if (!bruker && !feil) {
+  useEffect(() => {
+    const hentBrukerData = async () => {
+      try {
+        const uId = await userUId; // Vente på IDen
+        console.log("uID: (page) " + uId);
+        setBruker(uId);
+      } catch (error) {
+        setFeil(error); // Håndter feil
+        console.error(error);
+      }
+    };
+
+    hentBrukerData();
+  }, []);
+
+  if (!bruker && !feil) {
     return (
-        <div>
-            <h1>Ingen tilgang</h1>
-            <p>Vennligst logg inn og forsøk på nytt.</p>
+      <div className={styles.containerLoggedOut}>
+        <div className={styles.contentLoggedOut}>
+          <h1>Ingen tilgang</h1>
+          <p>Vennligst logg inn og forsøk på nytt.</p>
         </div>
+      </div>
     );
-    }
+  }
 
-
-    return (
-    <div>
-        <div>
-            <AlleAnsatte bedriftId={bruker} />
-        </div>
-
+  return (
+    <div className={styles.containerLoggedIn}>
+      <div className={styles.contentLoggedIn}>
+        <AlleAnsatte bedriftId={bruker} />
+      </div>
     </div>
-    )};
-        /*div>
+  );
+}
+/*div>
         
       <div>
       {userList.map((user) => (
